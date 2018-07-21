0972433cc8b3cd875f76c0fc50e57317cfe9a151
7g0r <40136311+7g0r@users.noreply.github.com>
2018-07-21 11:05:39 +0200

    New Tests

--- srcTest/StringCalculatorTest.java
+++ srcTest/StringCalculatorTest.java
@@ -1,15 +1,37 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
-    
+
     @org.junit.jupiter.api.Test
-    public void shouldReturn0WhenStringIsEmpty(){
+    public void shouldReturn0WhenStringIsEmpty() {
         // given 
         StringCalculator stringCalculator = new StringCalculator();
         //when
         int result = stringCalculator.add("");
         //then 
         assertEquals(0, result);
+    }
+
+    @Test
+    public void shouldReturn1() {
+        // given 
+        StringCalculator stringCalculator = new StringCalculator();
+        //when 
+        int result = stringCalculator.add("1");
+        //then 
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldReturnSumOfTwoStrings() {
+        // given
+        StringCalculator stringCalculator = new StringCalculator()
+        //when
+        int result = stringCalculator.add("1,2");
+        //then
+        assertEquals(3, result);
     }
 
 }
