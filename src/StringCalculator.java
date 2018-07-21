c4e4a35ac8664fdae0415a51fc35a3c76e4b739a
monaduz <duzmonika@gmail.com>
2018-07-21 11:33:50 +0200

    commit 2

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,13 +1,28 @@
 public class StringCalculator {
 
     public int add(String s) {
-        if (s==null) {
+        if (s.equals( "" )) {
             return 0;
+        } else if (s.length() == 1) {
+            int singleNum = Integer.parseInt( s );
+            return singleNum;
         }
-        return -1;
+
+        String[] arrayOfNumbers = s.split( "," );
+     //   int sum = 0;
+     //   int parsedNumber;
+
+        int element = Integer.parseInt(  arrayOfNumbers[0]);
+        int element2 = Integer.parseInt(  arrayOfNumbers[1]);
+
+//        for (String number : arrayOfNumbers) {
+//            parsedNumber = Integer.parseInt( number );
+//            sum += parsedNumber;
+//        }
+        return element+element2;
+
+
+
+
     }
-
-
-
-
 }
